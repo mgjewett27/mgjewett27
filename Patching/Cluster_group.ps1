cd8fd9a4b7d4425bfb67f7a640b51388392ba0bc
mgjewett27 <mgjewett27@gmail.com>
2025-04-09 00:09:51 -0400

    Create Cluster_group.ps1

---
+++ Patching/Cluster_group.ps1
@@ -0,0 +1,7 @@
+﻿Set-ExecutionPolicy Unrestricted
+$cluster_group = Get-ClusterGroup -Name "SQL Server*", "*_AG*", "*AG_*" | Select -ExpandProperty OwnerNode | Select -ExpandProperty Name
+$domain_name = Get-WmiObject -Namespace root\cimv2 -Class Win32_ComputerSystem | Select -ExpandProperty Domain
+
+$FQDN_cluster = "$cluster_group.$domain_name"
+
+$FQDN_cluster
